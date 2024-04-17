14c2f033ed45290cbbc9017946e43032527c78ec
Pepe <a01620480@itesm.mx>
2024-04-17 17:39:06 -0600

    new

--- student/a016204080_student_turtle.cpp
+++ student/a016204080_student_turtle.cpp
@@ -6,7 +6,7 @@
  * ANDREW ID:
  * LAST UPDATE:
  *
- * This file is an algorithm to solve the csrtle maze
+ * This file is an algorithm to solve the statertle maze
  * using the left-hand rule. The code is intentionaly left obfuscated.
  *
  */
@@ -24,17 +24,19 @@ enum Orientation
 };
 
 enum States
-{
-
-}
+{	
+	kBlocked = 0,
+	kUnkown = 1,
+	kReadyToMove = 2,
+};
 
 // OK TO MODIFY BELOW THIS LINE
 
-#define TIMEOUT 40 // bigger number slows down simulation so you can see what's happening
-float w,
-	cs;
+#define TIMEOUT 1 // bigger number slows down simulation so you can see what's happening
+float w, state;
 float current_x, current_y, next_x, next_y;
-float z, is_at_end, mod, is_bumped, q;
+float ready_to_move, is_at_end, is_bumped;
+// q and mode
 
 // this procedure takes the current turtle position and orientation and returns
 // true=submit changes, false=do not submit changes
@@ -43,75 +45,37 @@ float z, is_at_end, mod, is_bumped, q;
 bool studentMoveTurtle(QPointF &current_position, int &new_orientation)
 {
 	ROS_INFO("Turtle update Called  w=%f", w);
-	mod = true;
 	if (w == 0)
 	{
 		current_x = current_position.x();
 		current_y = current_position.y();
 		next_x = current_position.x();
 		next_y = current_position.y();
-		if (new_orientation == kFoward)
+		if (new_orientation == Orientation::kFoward)
 			next_y += 1;
 		else if (new_orientation == kRight)
 			next_x += 1;
 		is_bumped = bumped(current_x, current_y, next_x, next_y);
 		is_at_end = atend(current_position.x(), current_position.y());
-		if (new_orientation == 0)
-			if (cs == 2)
-			{
-				new_orientation = 3;
-				cs = 1;
-			}
-			else if (is_bumped)
-			{
-				new_orientation = 1;
-				cs = 0;
-			}
-			else
-				cs = 2;
-		else if (new_orientation == 1)
-			if (cs == 2)
-			{
-				new_orientation = 0;
-				cs = 1;
-			}
-			else if (is_bumped)
-			{
-				new_orientation = 2;
-				cs = 0;
-			}
-			else
-				cs = 2;
-		else if (new_orientation == 2)
-			if (cs == 2)
-			{
-				new_orientation = 1;
-				cs = 1;
-			}
-			else if (is_bumped)
-			{
-				new_orientation = 3;
-				cs = 0;
-			}
-			else
-				cs = 2;
-		else if (new_orientation == 3)
-			if (cs == 2)
-			{
-				new_orientation = 2;
-				cs = 1;
-			}
-			else if (is_bumped)
-			{
-				new_orientation = 0;
-				cs = 0;
-			}
-			else
-				cs = 2;
-		ROS_INFO("Orientation=%f  STATE=%f", new_orientation, cs);
-		z = cs == 2;
-		mod = true;
-		if (z == true && is_at_end == false)
+		
+		// if it is ready to move, means it is a new cell
+		if (state == States::kReadyToMove){
+			// set the state to unknown
+			state = States::kUnkown;
+			// turn left
+			new_orientation = (new_orientation + 3) % 4;
+		} else if (is_bumped){
+			// turn left
+			new_orientation = (new_orientation + 1) % 4;
+			// set the state to blocked
+			state = States::kBlocked;
+		} else {
+			// set the state to ready to move
+			state = States::kReadyToMove;
+		}
+		ROS_INFO("Orientation=%f  STATE=%f", new_orientation, state);
+		ready_to_move = state == 2;
+		if (ready_to_move == true && is_at_end == false)
 		{
 			if (new_orientation == 1)
 				current_position.setY(current_position.y() - 1);
@@ -122,7 +86,6 @@ bool studentMoveTurtle(QPointF &current_position, int &new_orientation)
 			if (new_orientation == 0)
 				current_position.setX(current_position.x() - 1);
 			z = false;
-			mod = true;
 		}
 	}
 	if (is_at_end)
