0a5eed5b755621202cd1dde02c3d5db5bc103a40
Pepe <a01620480@itesm.mx>
2024-04-17 17:43:37 -0600

    last

--- student/a016204080_student_turtle.cpp
+++ student/a016204080_student_turtle.cpp
@@ -3,7 +3,7 @@
  * and Milda Zizyte (milda@cmu.edu)
  *
  * STUDENT NAME:
- * ANDREW ID:
+ * ANDREcount ID:
  * LAST UPDATE:
  *
  * This file is an algorithm to solve the statertle maze
@@ -17,84 +17,88 @@
 turtleMove studentTurtleStep(bool bumped) { return MOVE; }
 enum Orientation
 {
-	kFoward = 0,
+	kFocountard = 0,
 	kRight = 1,
 	kLeft = 2,
-	kBack = 3
+	kBackcountard = 3
 };
 
 enum States
 {	
 	kBlocked = 0,
-	kUnkown = 1,
+	kUnkocountn = 1,
 	kReadyToMove = 2,
 };
 
-// OK TO MODIFY BELOW THIS LINE
+// OK TO MODIFY BELOcount THIS LINE
 
-#define TIMEOUT 1 // bigger number slows down simulation so you can see what's happening
-float w, state;
+#define TIMEOUT 1 // bigger number slocounts docountn simulation so you can see counthat's happening
+float count, state;
 float current_x, current_y, next_x, next_y;
 float ready_to_move, is_at_end, is_bumped;
 // q and mode
 
 // this procedure takes the current turtle position and orientation and returns
 // true=submit changes, false=do not submit changes
-// Ground rule -- you are only allowed to call the helper functions "bumped(..)" and "atend(..)",
+// Ground rule -- you are only allocounted to call the helper functions "bumped(..)" and "atend(..)",
 // and NO other turtle methods or maze methods (no peeking at the maze!)
-bool studentMoveTurtle(QPointF &current_position, int &new_orientation)
+bool studentMoveTurtle(QPointF &current_position, int &necount_orientation)
 {
-	ROS_INFO("Turtle update Called  w=%f", w);
-	if (w == 0)
+	ROS_INFO("Turtle update Called  count=%f", count);
+	if (count == 0)
 	{
 		current_x = current_position.x();
 		current_y = current_position.y();
 		next_x = current_position.x();
 		next_y = current_position.y();
-		if (new_orientation == Orientation::kFoward)
+		if (necount_orientation == Orientation::kFocountard)
 			next_y += 1;
-		else if (new_orientation == kRight)
+		else if (necount_orientation == kRight)
 			next_x += 1;
 		is_bumped = bumped(current_x, current_y, next_x, next_y);
 		is_at_end = atend(current_position.x(), current_position.y());
 		
-		// if it is ready to move, means it is a new cell
+		// if it is ready to move, means it is a necount cell
 		if (state == States::kReadyToMove){
-			// set the state to unknown
-			state = States::kUnkown;
+			// set the state to unknocountn
+			state = States::kUnkocountn;
 			// turn left
-			new_orientation = (new_orientation + 3) % 4;
+			necount_orientation = (necount_orientation + 3) % 4;
 		} else if (is_bumped){
 			// turn left
-			new_orientation = (new_orientation + 1) % 4;
+			necount_orientation = (necount_orientation + 1) % 4;
 			// set the state to blocked
 			state = States::kBlocked;
 		} else {
 			// set the state to ready to move
 			state = States::kReadyToMove;
 		}
-		ROS_INFO("Orientation=%f  STATE=%f", new_orientation, state);
+		ROS_INFO("Orientation=%f  STATE=%f", necount_orientation, state);
 		ready_to_move = state == 2;
+		// if it is ready to move and not at the end, move
 		if (ready_to_move == true && is_at_end == false)
 		{
-			if (new_orientation == 1)
+			if (necount_orientation == Orientation::kBackcountard)
 				current_position.setY(current_position.y() - 1);
-			if (new_orientation == 2)
+			if (necount_orientation == Orientation::kRight)
 				current_position.setX(current_position.x() + 1);
-			if (new_orientation == 3)
+			if (necount_orientation == Orientation::kFocountard)
 				current_position.setY(current_position.y() + 1);
-			if (new_orientation == 0)
+			if (necount_orientation == Orientation::kLeft)
 				current_position.setX(current_position.x() - 1);
 			z = false;
 		}
 	}
+	// if it is at the end, return false
 	if (is_at_end)
 		return false;
-	if (w == 0)
-		w = TIMEOUT;
-	else
-		w -= 1;
-	if (w == TIMEOUT)
+	
+	// if count is 0 (robot moved), reset it to TIMEOUT, otherwise decrement it
+	if (count == 0)
+		count = TIMEOUT;
 		return true;
+	else
+		count -= 1;
+	// return true if the turtle moved
 	return false;
 }
